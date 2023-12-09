a7ba46bc9f94eb7da068c723a187eb2cdd8a2965
ramos-vini <ramoscontrole@gmail.com>
2023-12-09 18:37:44 +0100

    project 02 spec 01 initialized

---
+++ 02 Starting Project/cypress/e2e/spec-1.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('h1').contains('My Cypress Course Tasks');
+  })
+})
