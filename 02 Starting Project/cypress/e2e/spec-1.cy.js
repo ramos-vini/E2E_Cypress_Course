e567498289973ff38a0ee9ebd26c3a71825c7dd8
Vinicius Ramos <viniciusramos@MBPvonVinicius.localdomain>
2023-12-11 13:23:45 +0100

    Testing length of h1 elements array

--- 02 Starting Project/cypress/e2e/spec-1.cy.js
+++ 02 Starting Project/cypress/e2e/spec-1.cy.js
@@ -3,6 +3,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:5173/');
+    cy.get('h1').should('have.length', 1);
     cy.get('h1').contains('My Cypress Course Tasks');
   })
 })
