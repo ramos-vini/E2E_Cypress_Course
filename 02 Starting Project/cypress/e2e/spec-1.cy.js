b01526321ad4e32a57f9cf6c11f0a3c91ce6634e
ramos-vini <ramoscontrole@gmail.com>
2023-12-12 22:08:12 +0100

    get() vs find()

--- 02 Starting Project/cypress/e2e/spec-1.cy.js
+++ 02 Starting Project/cypress/e2e/spec-1.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('main page', () => {
+  it('has a title', () => {
     cy.visit('http://localhost:5173/');
     cy.get('h1').should('have.length', 1);
     cy.get('h1').contains('My Cypress Course Tasks');
+  })
+
+  it('has an image in the header', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('.main-header img'); // cy.get('.main-header').find('img');
   })
 })
