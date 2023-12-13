d9d2205fd7d25551ffe19c0283a955f44f3fec5f
ramos-vini <ramoscontrole@gmail.com>
2023-12-13 23:22:08 +0100

    Closing the modal with 'Cancel' button

--- 02 Starting Project/cypress/e2e/tasks.cy.js
+++ 02 Starting Project/cypress/e2e/tasks.cy.js
@@ -6,10 +6,17 @@ describe("tasks management", () => {
     
     it('should open and close the new task modal', () => {
         cy.visit('http://localhost:5173/');
-        cy.get('button').contains('Add Task').click();
-        cy.get('.backdrop').click({force: true});
-        cy.get('.backdrop').should('not.exist');
-        cy.get('dialog.modal').should('not.exist');
+
+            cy.get('button').contains("Add Task").click();
+            cy.get('.backdrop').click({force: true});
+            cy.get('.backdrop').should('not.exist');
+            cy.get('dialog.modal').should('not.exist');
+
+            cy.get('button').contains('Add Task').click();
+            cy.get('button').contains('Cancel').click();
+            cy.get('.backdrop').should('not.exist');
+            cy.get('dialog.modal').should('not.exist');
+
     });
 }
 )
