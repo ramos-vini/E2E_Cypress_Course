72333e146bd608b7807bbfff25bd829b62a349d0
ramos-vini <ramoscontrole@gmail.com>
2023-12-26 19:22:30 +0100

    it should add multiple tasks

--- 02 Starting Project/cypress/e2e/tasks.cy.js
+++ 02 Starting Project/cypress/e2e/tasks.cy.js
@@ -47,7 +47,7 @@ describe("tasks management", () => {
         cy.get('button').contains('Add Task').click();
         cy.get('#title').type("New Task");
         cy.get('#summary').type('Some Description');
-        cy.get('#category').select('urgent')
+        cy.get('#category').select('urgent');
         cy.get('.modal button').contains('Add Task').click();
         cy.get('.task').should('have.length', 1);
 
@@ -57,6 +57,26 @@ describe("tasks management", () => {
         cy.get('#filter').select('urgent');
         cy.get('.task').should('have.length', 1);
     });
+
+    it('should add multiple tasks', () => {
+        cy.visit('http://localhost:5173/');
+
+        cy.get('button').contains('Add Task').click();
+        cy.get('#title').type("Task 1");
+        cy.get('#summary').type('First Task');
+        cy.get('.modal button').contains('Add Task').click();
+        cy.get('.task').should('have.length', 1);
+
+        cy.get('button').contains('Add Task').click();
+        cy.get('#title').type("Task 2");
+        cy.get('#summary').type('Second Task');
+        cy.get('.modal button').contains('Add Task').click();
+        cy.get('.task').should('have.length', 2);
+
+        cy.get('.task').eq(0).find('h2').contains('Task 1'); // first()
+        cy.get('.task').eq(1).find('h2').contains('Task 2'); // last()
+    });
+    
     
 }
 )
