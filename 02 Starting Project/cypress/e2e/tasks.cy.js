4c777890b04d7c29facf7fa3c8a4a938ef2b07c4
ramos-vini <ramoscontrole@gmail.com>
2023-12-26 18:31:16 +0100

    tasks.cy.js identation

--- 02 Starting Project/cypress/e2e/tasks.cy.js
+++ 02 Starting Project/cypress/e2e/tasks.cy.js
@@ -3,19 +3,19 @@ describe("tasks management", () => {
         cy.visit('http://localhost:5173/');
         cy.get('button').contains('Add Task').click();
     });
-    
+
     it('should open and close the new task modal', () => {
         cy.visit('http://localhost:5173/');
 
-            cy.get('button').contains("Add Task").click();
-            cy.get('.backdrop').click({force: true});
-            cy.get('.backdrop').should('not.exist');
-            cy.get('dialog.modal').should('not.exist');
+        cy.get('button').contains("Add Task").click();
+        cy.get('.backdrop').click({ force: true });
+        cy.get('.backdrop').should('not.exist');
+        cy.get('dialog.modal').should('not.exist');
 
-            cy.get('button').contains('Add Task').click();
-            cy.get('button').contains('Cancel').click();
-            cy.get('.backdrop').should('not.exist');
-            cy.get('dialog.modal').should('not.exist');
+        cy.get('button').contains('Add Task').click();
+        cy.get('button').contains('Cancel').click();
+        cy.get('.backdrop').should('not.exist');
+        cy.get('dialog.modal').should('not.exist');
 
     });
 }
